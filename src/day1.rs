2e5a29d24514c405acbdab582aebc5905b6cfe62
Kevin Murray <kevin.murray@omnihotels.com>
2019-12-02 16:16:31 -0600

    Day 1 commit adding unit tests

--- src/day1.rs
+++ src/day1.rs
@@ -15,8 +15,8 @@ pub fn day1() -> io::Result<()> {
         total_fuel += fuel_load;
         module_fuel += get_wishful_load(fuel_load);
     }
-    println!("total fuel {}", total_fuel);
-    println!("module fuel {}", module_fuel);
+    println!("Day 1 total fuel {}", total_fuel);
+    println!("Day 1 module fuel {}", module_fuel);
 
     Ok(())
 
@@ -42,4 +42,41 @@ fn get_wishful_load(fuel_load: i32) -> i32 {
         }
     }
     mod_sum
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::day1::get_fuel_load;
+    use crate::day1::get_wishful_load;
+
+    #[test]
+    fn example_1() {
+        assert_eq!(get_fuel_load(12), 2);
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(get_fuel_load(14), 2);
+    }
+
+    #[test]
+    fn example_3() {
+        assert_eq!(get_fuel_load(1969), 654);
+    }
+
+    #[test]
+    fn example_4() {
+        assert_eq!(get_fuel_load(100756), 33583);
+    }
+
+    #[test]
+    fn example_5() {
+        assert_eq!(get_wishful_load(654), 966);
+    }
+
+    #[test]
+    fn example_6() {
+        assert_eq!(get_wishful_load(33583), 50346);
+    }
+
 }
