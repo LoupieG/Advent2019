9500a32b9c573ddec16671c7a78452981b5a1536
Kevin Murray <garou29@yahoo.com>
2019-12-02 21:00:20 -0600

    Day 2 part 2

--- src/day2.rs
+++ src/day2.rs
@@ -6,7 +6,7 @@ pub fn day2() -> io::Result<()> {
     let file = File::open("./input/Day2.txt")?;
     let reader = BufReader::new(file);
 
-    let mut numVec: Vec<i32> = reader
+    let numVec: Vec<i32> = reader
         .lines()
         .next()
         .unwrap()
@@ -16,8 +16,23 @@ pub fn day2() -> io::Result<()> {
         .map(|s| s.parse().unwrap())
         .collect();
 
-    calculate_vector(&mut numVec);
-    println!("Day 2 part 1 = {}", numVec[0]);
+    let mut p1Vec = numVec.to_vec();
+    calculate_vector(&mut p1Vec);
+
+    for x in 0..99 {
+        for y in 0..99 {
+           let mut p2Vec = numVec.to_vec();
+           p2Vec[1] = x;
+           p2Vec[2] = y;
+           calculate_vector(&mut p2Vec);
+            if p2Vec[0].eq(19690720.borrow()) {
+               println!("{} {} {} total {}", p2Vec[0], p2Vec[1], p2Vec[2], (p2Vec[1] * 100) + p2Vec[2]);
+               break;
+           }
+        }
+    }
+
+    println!("Day 2 part 1 = {}", p1Vec[0]);
     Ok(())
 
 }
