b17392354b8098acb096658643366370c2035f8c
Kevin Murray <garou29@yahoo.com>
2019-12-05 06:07:54 -0600

    Day 2 using match

--- src/day2.rs
+++ src/day2.rs
@@ -40,14 +40,11 @@ pub fn day2() -> io::Result<()> {
 fn calculate_vector(iVec: &mut Vec<i32>) {
     for index in (0..iVec.len() - 1).step_by(4) {
         let loc = iVec[index + 3] as usize;
-        if iVec[index].eq(1.borrow()) {
-           iVec[loc] = iVec[iVec[index + 1] as usize] + iVec[iVec[index + 2] as usize];
-        }
-        else if iVec[index].eq(2.borrow()) {
-            iVec[loc] = iVec[iVec[index + 1] as usize] * iVec[iVec[index + 2] as usize];
-        }
-        else if iVec[index].eq(99.borrow()) {
-            break;
+        iVec[loc] = match iVec[index] {
+            1 => iVec[iVec[index + 1] as usize] + iVec[iVec[index + 2] as usize],
+            2 => iVec[iVec[index + 1] as usize] * iVec[iVec[index + 2] as usize],
+            99 => break,
+            _ => break,
         }
     }
 }
